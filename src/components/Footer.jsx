88b1cadaf12cf3faa05774cdaf7336d702b6a5af
Goce Mitevski <goce.mitevski@gmail.com>
2025-06-14 18:40:44 +0200

    Refactor Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,31 +4,36 @@ export default function Footer() {
       <footer className="container-fluid">
         <div className="row">
           <div className="col-lg-10 offset-lg-1">
-            Изработено од{" "}
-            <a
-              className="link-light"
-              rel="noopener"
-              href="https://www.gocemitevski.com/"
-            >
-              Гоце Митевски
-            </a>
-            .{" "}
-            <a
-              className="link-light"
-              rel="noopener"
-              href="https://github.com/gocemitevski/registar-licenci-ugostitelska-dejnost"
-            >
-              Изворниот код
-            </a>{" "}
-            е достапен на GitHub под лиценцата „
-            <a
-              className="link-light"
-              rel="noopener"
-              href="https://github.com/gocemitevski/registar-licenci-ugostitelska-dejnost?tab=GPL-3.0-1-ov-file"
-            >
-              GPL-3.0
-            </a>
-            “.
+            <div className="row">
+              <div className="col">
+                <a
+                  className="link-light"
+                  rel="noopener"
+                  href="https://github.com/gocemitevski/registar-licenci-ugostitelska-dejnost"
+                >
+                  Изворниот код
+                </a>{" "}
+                е достапен на GitHub под лиценцата „
+                <a
+                  className="link-light"
+                  rel="noopener"
+                  href="https://github.com/gocemitevski/registar-licenci-ugostitelska-dejnost?tab=GPL-3.0-1-ov-file"
+                >
+                  GPL-3.0
+                </a>
+                “.
+              </div>
+              <div className="col-auto">
+                Изработено од{" "}
+                <a
+                  className="link-light"
+                  rel="noopener"
+                  href="https://www.gocemitevski.com/"
+                >
+                  Гоце Митевски
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
