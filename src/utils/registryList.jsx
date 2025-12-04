fc19847cd26a1eb2beac9c0e47b41eb5762e65f0
Goce Mitevski <goce.mitevski@gmail.com>
2025-12-04 19:01:16 +0100

    Add registryList

---
+++ src/utils/registryList.jsx
@@ -0,0 +1,18 @@
+export const registryList = [
+  {
+    date: "4 декември 2025",
+    file: "registar-n-izdadeni-licenci-za-vrsenje-na-ugostitelska-dejnost-kabare-nokjen-bar-diskoklub-i-diskoklub-na-otvoren-prostor-file-Cz34.xlsx",
+  },
+  {
+    date: "26 август 2025",
+    file: "registar-n-izdadeni-licenci-za-vrsenje-na-ugostitelska-dejnost-kabare-nokjen-bar-diskoklub-i-diskoklub-na-otvoren-prostor-file-6grd.xlsx",
+  },
+  {
+    date: "30 јули 2025",
+    file: "Регистар Лиценци (2025 год)_290725.xlsx",
+  },
+  {
+    date: "30 април 2025",
+    file: "Copy of Регистар Лиценци (2025 год)_ok.xlsx",
+  },
+];
