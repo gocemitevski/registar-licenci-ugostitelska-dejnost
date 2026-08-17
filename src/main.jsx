58473baf4f79047e495b172ac7cd21eabaf12a21
Goce Mitevski <goce.mitevski@gmail.com>
2026-08-17 18:36:01 +0200

    Update cookieDomain

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Cookies } from "react-cookie-consent";
 if (Cookies.get(import.meta.env.VITE_APP_GA_ID) === "true") {
   import.meta.env.VITE_APP_GA &&
     ReactGA.initialize(import.meta.env.VITE_APP_GA, {
-      gaOptions: { cookieDomain: "gocemitevski.github.io" },
+      gaOptions: { cookieDomain: "licenci.gocemitevski.com" },
     });
 }
 
